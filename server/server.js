ac2a4a45b6f085e81a75cd59f6843f6980fbda4d
zigm <77489316+orgxtopaz@users.noreply.github.com>
2021-11-29 18:06:21 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ require("dotenv").config();
 let User = require("./models/user_model");
 
 const app = express();
-app.use(express.json({limit: "30mb",extended:true}));
-// app.use(express.urlencoded({limit: "30mb",extended:true}));
-app.use(cors())
+ app.use(cors({
+    origin: `https://orgclient.herokuapp.com/`,  //react's address
+    credentials: true
+}));
+app.use(express.json()); // app use express.json
 // FOR THE VALIDATIONS
 const { body, validationResult } = require("express-validator");
 
